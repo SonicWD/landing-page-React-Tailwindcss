2c149f76c97ded071724184cd2950ad04043f879
LuxoJR-WD <diazcapadorw@hotmail.com>
2024-05-30 17:00:42 -0400

    Add .gitattributes to handle line endings

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+//import React from "react"
+
+ const NavBar = () => {
+    const links = [
+       {
+        id: 1,
+        src: "Inicio"
+       },
+       {
+        id: 2,
+        src: "Productos"
+       },
+       {
+        id: 3,
+        src: "Promociones"
+       },
+       {
+        id: 4,
+        src: "Acerca de"
+       }
+
+    ]
+ return <div className="w-screen fixed h-20 border border-x-cyan-100">NavBar</div>
+}
+
+export default NavBar;
