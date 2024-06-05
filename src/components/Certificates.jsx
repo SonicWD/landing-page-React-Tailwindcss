43009b7fc5485881a15f15dd08db816f26567b6f
LuxoJR-WD <diazcapadorw@hotmail.com>
2024-06-05 17:07:36 -0400

    Se arreglan problemas de optimizacion

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -7,37 +7,37 @@ const Certificates = () => {
         <footer className="flex flex-wrap justify-center md:justify-around items-stretch bg-gray-900 py-5 border-t-4 border-b-4 border-black">
             {/* Certificado 1 */}
             <CertificateItem
-                imageUrl="https://hidegar.com/wp-content/uploads/2019/02/ISO-9001.png"
+                imageUrl="/src/images/certificates/certificate1.png"
                 altText="Normas ISO"
                 description=""
             />
             {/* Certificado 2 */}
             <CertificateItem
-                imageUrl="https://secure.porcicultura.com/uploads/Upload-5b2d111da349f-22062018.jpeg"
+                imageUrl="/src/images/certificates/certificate2.jpeg"
                 altText="Normas ISO"
                 description=""
             />
             {/* Certificado 3 */}
             <CertificateItem
-                imageUrl="https://vectorise.net/logo/wp-content/uploads/2010/08/Logo-IQNet-Certified-Management-System.png"
+                imageUrl="/src/images/certificates/certificate3.png"
                 altText="Certificado IQNet"
                 description=""
             />
             {/* Certificado 4 */}
             <CertificateItem
-                imageUrl="https://www.rapunzel.de/img/logo-handinhand.png"
+                imageUrl="/src/images/certificates/certificate4.png"
                 altText="100% orgánico"
                 description=""
             />
             {/* Certificado 5 */}
             <CertificateItem
-                imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/USDA_organic_seal.svg/500px-USDA_organic_seal.svg.png"
+                imageUrl="/src/images/certificates/certificate5.png"
                 altText="Aval USDA"
                 description=""
             />
             {/* Certificado 6 */}
             <CertificateItem
-                imageUrl="https://spp.coop/wp-content/uploads/2023/06/cropped-ES-SPP-PP-CJ_2023-06-27_APR_crvs-01-300x300.png"
+                imageUrl="/src/images/certificates/certificate6.png"
                 altText="SPP"
                 description=""
             />
