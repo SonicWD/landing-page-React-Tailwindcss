97bc03eb3dd2671307917909dcd438d819d8ce46
LuxoJR-WD <diazcapadorw@hotmail.com>
2024-06-11 10:33:53 -0400

    Se arregla problemas

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,16 +1,11 @@
 // MapComponent.jsx
 // eslint-disable-next-line no-unused-vars
-// MapComponent.jsx
-// eslint-disable-next-line no-unused-vars
 import React, { useEffect } from 'react';
 
 const MapComponent = () => {
   useEffect(() => {
-    // Función para inicializar y cargar el mapa
     function initMap() {
-      // Ubicación del mapa
       var location = { lat: -14.831961631774902, lng: -64.90095520019531 };
-      // Crear un mapa nuevo
       // eslint-disable-next-line no-unused-vars
       var map = new window.google.maps.Map(document.getElementById("map"), {
         zoom: 14,
@@ -18,15 +13,31 @@ const MapComponent = () => {
       });
     }
 
-    // Cargar la API de Google Maps
+    const existingScript = document.querySelector('script[src^="https://maps.googleapis.com/maps/api/js"]');
+    if (existingScript) {
+      console.warn('El script de Google Maps ya está cargado.');
+      if (window.google && window.google.maps) {
+        initMap();
+      } else {
+        existingScript.addEventListener('load', initMap);
+      }
+      return;
+    }
+
     const script = document.createElement('script');
     const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+    if (!apiKey) {
+      console.error('No se encontró la clave de la API de Google Maps. Asegúrate de que la variable de entorno VITE_GOOGLE_MAPS_API_KEY esté configurada correctamente.');
+      return;
+    } else {
+      console.log('Clave de API de Google Maps cargada correctamente.');
+    }
     script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+    script.async = true;
     script.defer = true;
-    window.initMap = initMap; // Hacer que la función initMap esté disponible globalmente
+    window.initMap = initMap;
     document.head.appendChild(script);
 
-    // Limpiar el script al desmontar el componente
     return () => {
       document.head.removeChild(script);
       delete window.initMap;
@@ -37,3 +48,4 @@ const MapComponent = () => {
 };
 
 export default MapComponent;
+
